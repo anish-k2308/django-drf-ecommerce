7895e3cbf09d40aa5b72893179762c66afa42a2e
anish-k2308 <85450660+anish-k2308@users.noreply.github.com>
2024-06-04 11:35:28 +0530

    Update models.py

--- ecommerce/ecommerce/products/models.py
+++ ecommerce/ecommerce/products/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Product(models.Model):
@@ -15,3 +22,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
