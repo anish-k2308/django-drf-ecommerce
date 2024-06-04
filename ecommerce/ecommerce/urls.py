769f02fe3d4c4560e9f6cddf4bffbd15684b6f06
anish-k2308 <85450660+anish-k2308@users.noreply.github.com>
2024-06-04 12:57:12 +0530

    Update urls.py

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+routers = DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(routers.urls)),
 ]
