8fbee7022864a1cda9ebb7a48a16607c7f862a84
anish-k2308 <85450660+anish-k2308@users.noreply.github.com>
2024-06-04 12:53:28 +0530

    serializer created and viewset created

--- ecommerce/ecommerce/products/views.py
+++ ecommerce/ecommerce/products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, Res
+from rest_framework import viewsets,response
+from serializers import *
+from models import *
 # Create your views here.
+
+
+class CategoryViewset(viewsets.ViewSet):
+    querryset = Category.objects.all()
+    def list(self, request):
+        serializer = CategorySerializer(self.querryset,many=True)
+        return response.Response(serializer.data)
